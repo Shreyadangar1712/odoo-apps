b34ec16b7ff146a175a3cc9b703cdfe6932ff8f8
Shreyadangar1712 <shreya@rishvi.co.uk>
2026-03-12 09:11:30 +0000

    Update __manifest__.py

--- hotel_management_systemmm/__manifest__.py
+++ hotel_management_systemmm/__manifest__.py
@@ -65,7 +65,7 @@
     ],
 
     'images': ['static/description/banner.png'],
-    'license': 'OPL-1',
+    'license': 'AGPL-3',
     'installable': True,
     'auto_install': False,
     'application': True,
