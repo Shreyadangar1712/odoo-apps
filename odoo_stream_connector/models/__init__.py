7cea14f76ffcaf5229064cd40bc63dd1e16c98d0
shreya849 <shreya@rishvi.co.uk>
2026-06-29 11:03:13 +0100

    final

--- odoo_stream_connector/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import sale_depot
-from . import sale_order_line
-from . import sale_order
-from . import res_company
-from . import sale_stream_services
-from . import run_details
-from . import sale_delivery
--- odoo_stream_connector/wizard/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import success_message
